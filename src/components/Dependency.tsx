3de0f1aaa3c4bad11a4daab819312330fd703de3
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-03 12:35:48 +0200

    Dependency

---
+++ src/components/Dependency.tsx
@@ -0,0 +1,14 @@
+// what we want: defaultCollapsed can only be true if collapsible is also true
+type PanelProps =
+  | { collapsible: true; defaultCollapsed?: boolean }
+  // this could replace the above
+  // | { collapsible: true; defaultCollapsed?: true }
+  | { collapsible?: never; defaultCollapsed?: never };
+
+export function Panel(props: PanelProps) {
+  return <pre>{JSON.stringify(props)}</pre>;
+}
+
+function DebugWhileDeveloping() {
+  return <Panel collapsible defaultCollapsed={false} />;
+}
